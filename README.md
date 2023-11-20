47d06a18d05986ea53e20374b0921861a838cb16
k.muruganantham <karthick.muruganantham@orbis.com>
2023-11-20 18:28:07 +0000

    update requirements.txt and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # hartree_test
-Hartree Data Processing Using Pandas and Beam
+
+# Hartree Data Processing Using Pandas and Beam
+
+## Project Description
+This project reads data from two CSV datasets, processes the data, and writes the results back to CSV files. There are two scripts, each using a different data processing framework to accomplish the task. The Apache Beam script is named `hartree_pblm_beam.py` and the Pandas script is named `hartree_pblm_pandas.py`.
+
+## Getting Started
+
+### Prerequisites
+The project requires Python 3.8 or higher, along with the Apache Beam and Pandas libraries. 
+
+You can install the prerequisites using pip:
+
+```shell
+pip install virtualenv
+```
+
+## Project Setup
+
+### Clone the Repository
+
+- Clone this repo to your local machine using terminal (macOS, Linux), or Command Prompt (Windows) with the following command:
+
+```shell
+git clone <this-repository-url>
+```
+
+### Create Virtual Environment
+
+- First navigate to your cloned project:
+
+```shell
+cd <path-to-cloned-project>
+```
+
+- Next, create a virtual environment, you could name the environment anything you want:
+
+```shell
+python -m venv myenv
+```
+### Activate Virtual Environment
+- To activate the virtual environment, navigate to the **Scripts** directory in the virtual environment folder you just created:
+
+  - on macOS and Linux:
+
+  ```shell
+  source myenv/bin/activate
+  ```
+
+  - on Windows:
+
+  ```shell
+  myenv\Scripts\activate
+  ```
+
+- Your shell prompt will now show the name of your virtualenv. 
+
+#### Install Required Python Packages
+
+- Install the Python packages that are required for this project, which are listed in `requirements.txt`.
+
+```shell
+pip install -r requirements.txt
+```
+
+## Run the Project
+
+- In your terminal, ensure you are in the root directory of the project. 
+  - To run the program with Apache Beam, type: 
+
+  ```shell
+  python hartree_pblm_beam.py
+  ```
+  - To run the program with the Pandas framework, type:
+
+  ```shell
+  python hartree_pblm_pandas.py
+  ```
+
+- The output will be a CSV file in the `data` directory.
--- hartree_pblm_beam.py
+++ hartree_pblm_beam.py
@@ -7,18 +7,22 @@ from collections import namedtuple
 K = TypeVar('K')
 V = TypeVar('V')
 
+
 def parse_file1(line):
     """ Parse Dataset1 """
     items = line.split(',')
     return items[2], items
+
 
 def parse_file2(line):
     """ Parse Dataset2 """
     items = line.split(',')
     return items[0], items
 
+
 def convert_num(row):
     return [int(field) if field.isdigit() else field for field in row]
+
 
 def parse_dataset(element) -> Tuple[K, V]:
     (key, data) = element
@@ -28,17 +32,8 @@ def parse_dataset(element) -> Tuple[K, V]:
         record = convert_num(record)
         yield ((record[1], record[2], dataset2[1]) , (record[3], record[4], record[5]))
 
-def calculate_aggregates(element):
-    (key, values) = element
-    counterparty = key
-    max_rating = max(values)
-    sum_value_ARAP = sum(value for rating, status, value, tier in values if status == 'ARAP')
-    sum_value_ACCR = sum(value for rating, status, value, tier in values if status == 'ACCR')
-    tier = values[0][3]
-    yield (counterparty, max_rating, sum_value_ARAP, sum_value_ACCR, tier)
 
-
-def calculate_sums(element):
+def calculate_aggs(element):
     (key, values) = element
     (legal_entity, counter_party, tier) = key 
     max_rating = max(map(lambda x: x[0], values))
@@ -50,8 +45,10 @@ def calculate_sums(element):
 
 Result = namedtuple('Result', ['legal_entity', 'counter_party', 'tier', 'rating', 'sum_ARAP', 'sum_ACCR'])
 
+
 def convert_to_namedtuple(elements):
     return Result(*elements)
+
 
 with beam.Pipeline(options=PipelineOptions()) as p:
     dataset1 = (
@@ -66,10 +63,11 @@ with beam.Pipeline(options=PipelineOptions()) as p:
         | "CustomerData To Tuple" >> beam.Map(parse_file2)
     )
 
-    aggregated = ({'dataset1': dataset1, 'dataset2': dataset2} 
+    aggregated = ({'dataset1': dataset1, 'dataset2': dataset2}
             | 'Combine datasets' >> beam.CoGroupByKey()
             | 'Convert Values' >> beam.FlatMap(parse_dataset)
             | 'Grouping Key' >> beam.GroupByKey()
-            | 'Calculate Sums' >> beam.Map(calculate_sums)
+            | 'Calculate aggregates' >> beam.Map(calculate_aggs)
             | 'Format output' >> beam.Map(convert_to_namedtuple)
-            | 'Final Output' >> beam.io.WriteToText('./data/beam_results', file_name_suffix='.csv'))
+            | 'Final Output' >> beam.io.WriteToText('./data/beam_results',
+                                                    file_name_suffix='.csv'))
